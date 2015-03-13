be9445ab793e645103ac2aeb484ce2be5609dd0b
Russell Aronson <russell.aronson@gmail.com>
2015-03-13 16:40:30 +1100

    Add more new line chars to N to filter out

--- src/main/scala/com/ambiata/disorder/N.scala
+++ src/main/scala/com/ambiata/disorder/N.scala
@@ -11,9 +11,12 @@ import GenPlus._
 case class N(value: String)
 
 object N {
+
+  val newlines: List[Char] = List('\n', '\r', 0x000B, 0x000C, 0x0085, 0x2028, 0x2029)
+
   implicit def NArbitrary: Arbitrary[N] =
     Arbitrary(for {
       a <- arbitrary[S]
-      c <- checkGen[Char](arbitrary[Char], "Char", x => x != '\n' && x != '\r')
-    } yield N(a.value.replace('\n', c).replace('\r', c)))
+      c <- checkGen[Char](arbitrary[Char], "Char", x => !newlines.contains(x))
+    } yield N(newlines.foldLeft(a.value)({ case (b, a) => b.replace(a, c) })))
 }
--- src/test/scala/com/ambiata/disorder/NSpec.scala
+++ src/test/scala/com/ambiata/disorder/NSpec.scala
@@ -10,5 +10,15 @@ class NSpec extends Specification with ScalaCheck { def is = s2"""
 
     ${ prop((n: N) => n.value.contains('\r') ==== false) }
 
+    ${ prop((n: N) => n.value.contains(0x000B) ==== false) }
+
+    ${ prop((n: N) => n.value.contains(0x000C) ==== false) }
+
+    ${ prop((n: N) => n.value.contains(0x0085) ==== false) }
+
+    ${ prop((n: N) => n.value.contains(0x2028) ==== false) }
+
+    ${ prop((n: N) => n.value.contains(0x2029) ==== false) }
+
 """
 }
